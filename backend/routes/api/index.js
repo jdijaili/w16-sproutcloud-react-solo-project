4dfd518f2339e18fe0391c2b40b5137a91f4a362
jdijaili <jennifer.dijailirx@gmail.com>
2022-01-06 12:12:52 -0800

    completed login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth  } = require('../../utils/auth.js');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-  });
+  res.json({ requestBody: req.body });
+});
+
 
 
 module.exports = router;
