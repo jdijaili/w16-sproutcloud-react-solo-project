913e8dcd2f09f156777effab63e24ebcf2f4eb20
jdijaili <jennifer.dijailirx@gmail.com>
2022-01-06 16:18:00 -0800

    made minor changes to nav and prof button componenets

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,47 +3,47 @@ import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
     };
 
-    document.addEventListener('click', closeMenu);
+    useEffect(() => {
+        if (!showMenu) return;
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+        document.addEventListener('click', closeMenu);
 
-  return (
-    <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
-      {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )}
-    </>
-  );
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    return (
+        <>
+            <button onClick={openMenu}>
+                <i className="fas fa-user-circle" />
+            </button>
+            {showMenu && (
+                <ul className="profile-dropdown">
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button onClick={logout}>Log Out</button>
+                    </li>
+                </ul>
+            )}
+        </>
+    );
 }
 
 export default ProfileButton;
