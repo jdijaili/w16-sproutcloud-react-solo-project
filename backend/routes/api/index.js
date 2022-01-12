49722f3356649d1fd6f99fa4f0fa08a40aa94476
jdijaili <jennifer.dijailirx@gmail.com>
2022-01-12 14:52:39 -0800

    removed extraneous code from routes index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,4 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser, requireAuth  } = require('../../utils/auth.js');
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
